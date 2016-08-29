faf81f83b6954edeec767b03fcb3fe4f6dfc90eb
nomad23541 <chrisreading.email@gmail.com>
2016-08-29 13:51:39 -0600

    Create model class for all food items

---
+++ src/com/chrisreading/coveis/model/Item.java
@@ -0,0 +1,63 @@
+package com.chrisreading.coveis.model;
+
+import java.io.Serializable;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *  Model for a food item sold in the cove
+ */
+public class Item implements Serializable {
+	private static final long serialVersionUID = -5962031618781287190L;
+	
+	private StringProperty name;
+	private DoubleProperty price;
+	private IntegerProperty amount;
+	
+	/**
+	 * Default constructor
+	 */
+	public Item() {
+		
+	}
+	
+	public Item(String name, double price, int amount) {
+		this.name = new SimpleStringProperty(name);
+		this.price = new SimpleDoubleProperty(price);
+		this.amount = new SimpleIntegerProperty(amount);
+	}
+	
+	/**
+	 * GETTERS & SETTERS
+	 */
+	
+	public void setName(String name) {
+		this.name.set(name);
+	}
+	
+	public String getName() {
+		return name.get();
+	}
+	
+	public void setPrice(double price) {
+		this.price.set(price);
+	}
+	
+	public double getPrice() {
+		return price.get();
+	}
+	
+	public void setAmount(int amount) {
+		this.amount.set(amount);
+	}
+	
+	public int getAmount() {
+		return amount.get();
+	}
+
+}
