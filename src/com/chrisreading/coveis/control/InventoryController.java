cab590e57e984d5c47328df548280af6a884298a
nomad23541 <chrisreading.email@gmail.com>
2016-08-30 10:21:42 -0600

    Create controller for inventory view

---
+++ src/com/chrisreading/coveis/control/InventoryController.java
@@ -0,0 +1,19 @@
+package com.chrisreading.coveis.control;
+
+import javafx.fxml.FXML;
+
+/**
+ * Controller class to handle InventoryView.fxml
+ */
+public class InventoryController {
+	
+	public InventoryController() {
+		
+	}
+
+	@FXML
+	public void initialize() {
+		
+	}
+	
+}
