c9b0b4463332e999d9d66167ca95e78cc5ee8a9a
nomad23541 <chrisreading.email@gmail.com>
2016-08-29 18:54:57 -0600

    Remove test code

--- src/com/chrisreading/coveis/CoveInventorySystem.java
+++ src/com/chrisreading/coveis/CoveInventorySystem.java
@@ -7,35 +7,11 @@ import com.chrisreading.coveis.model.Item;
 
 /**
  * Main class, program is initiated here
- * Temporary code below to make sure inventory system is working
  */
 public class CoveInventorySystem {
 	
 	public CoveInventorySystem() {
-		DataManager dm = new DataManager();
 		
-		try {
-			dm.load();
-		} catch (ClassNotFoundException | IOException e) {
-			e.printStackTrace();
-		}
-		
-		if(!dm.getItems().isEmpty()) {
-			for(Item item : dm.getItems()) {
-				System.out.println(item.getName() + ", " + item.getPrice() + ", " + item.getAmount());
-			}	
-		}
-		
-		dm.getItems().add(new Item("Hot Cheetos", 4.00, 23));
-		dm.getItems().add(new Item("Diet Pepsi", 1.23, 5));
-		
-		// currently getting notseriazable error for fx simpleproperty objects, need to fix
-		try {
-			dm.save();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	public static void main(String[] args) {
