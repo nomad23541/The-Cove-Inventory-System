d80a6fdea6d509a33b93762ccff74c2f0a40aec4
nomad23541 <chrisreading.email@gmail.com>
2016-08-29 18:55:45 -0600

    Create utility class for handling files

---
+++ src/com/chrisreading/coveis/util/FileUtils.java
@@ -0,0 +1,29 @@
+package com.chrisreading.coveis.util;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utils for file use
+ */
+public class FileUtils {
+
+	/**
+	 * Get files from a directory
+	 * @param dir Directory 
+	 * @return File array
+	 */
+	public static List<File> getFiles(File dir) {
+		List<File> results = new ArrayList<File>();
+		File[] files = dir.listFiles();
+		for(int i = 0; i < files.length; i++) {
+			if(files[i].isFile()) {
+				results.add(files[i]);
+			}
+		}
+		
+		return results;
+	}
+	
+}
