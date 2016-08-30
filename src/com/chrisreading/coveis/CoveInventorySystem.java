56c288b54f73f14864f9adc55bd331f9448eddad
nomad23541 <chrisreading.email@gmail.com>
2016-08-29 19:27:15 -0600

    Add example in comment

--- src/com/chrisreading/coveis/CoveInventorySystem.java
+++ src/com/chrisreading/coveis/CoveInventorySystem.java
@@ -1,17 +1,24 @@
 package com.chrisreading.coveis;
 
-import java.io.IOException;
-
-import com.chrisreading.coveis.handler.DataManager;
-import com.chrisreading.coveis.model.Item;
-
 /**
  * Main class, program is initiated here
  */
 public class CoveInventorySystem {
 	
 	public CoveInventorySystem() {
+		/* 
+		 * How items should be added/load and saved 
+		InventoryManager im = new InventoryManager();
+		DataManager dm = new DataManager();
 		
+		dm.load();
+		
+		im.addItem(new Item("Hot Cheetos", 1.23, 50));
+		im.addItem(new Item("Mountain Dew", 1.50, 33));
+		im.addItem(new Item("Swag", 50.00, 5));
+		
+		dm.save();
+		*/
 	}
 
 	public static void main(String[] args) {
