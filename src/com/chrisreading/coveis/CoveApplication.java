eaba568dd63fe343f2a174cf5ac929b298db343f
nomad23541 <chrisreading.email@gmail.com>
2016-08-29 21:53:59 -0600

    Create class to handle JavaFX GUI initialization

---
+++ src/com/chrisreading/coveis/CoveApplication.java
@@ -0,0 +1,47 @@
+package com.chrisreading.coveis;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class CoveApplication extends Application {
+
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle(Vars.APP_TITLE);
+		
+		initRootLayout();
+		showInventoryOverview();
+	}
+	
+	public void initRootLayout() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(CoveInventorySystem.class.getResource("view//RootLayout.fxml"));
+		rootLayout = (BorderPane) loader.load();
+		
+		Scene scene = new Scene(rootLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public void showInventoryOverview() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(CoveInventorySystem.class.getResource("view//InventoryOverview"));
+		AnchorPane pane = (AnchorPane) loader.load();
+		
+		// set pane to the center of rootlayout
+		rootLayout.setCenter(pane);
+		
+		// TODO: add controller
+	}
+
+}
