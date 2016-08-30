bc1ac854c2267e7dbe45eb1d9881c2d4aa5f52cf
READING <9505180@graniteschools.org>
2016-08-30 09:55:03 -0600

    Add InventoryManager and DataManager initialization

--- src/com/chrisreading/coveis/CoveInventorySystem.java
+++ src/com/chrisreading/coveis/CoveInventorySystem.java
@@ -1,13 +1,56 @@
 package com.chrisreading.coveis;
 
+import java.io.IOException;
+
+import com.chrisreading.coveis.handler.DataManager;
+import com.chrisreading.coveis.handler.InventoryManager;
+
 import javafx.application.Application;
 
 /**
  * Main class, program is initiated here
  */
 public class CoveInventorySystem {
+	
+	/** Instance of this class */
+	private static CoveInventorySystem instance;
+	
+	private InventoryManager im; // inventory manager used
+	private DataManager dm; // data manager used
+	
+	public CoveInventorySystem() throws ClassNotFoundException, IOException {
+		instance = this;
+		im = new InventoryManager();
+		dm = new DataManager();
+		dm.load(); // first time setup / load previous files
+	}
+	
+	/**
+	 * Get an instance of this class to access the managers
+	 * @return
+	 */
+	public static CoveInventorySystem getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * Get the InventoryManager
+	 * @return
+	 */
+	public InventoryManager getIM() {
+		return im;
+	}
+	
+	/**
+	 * Get the DataManager
+	 * @return
+	 */
+	public DataManager getDM() {
+		return dm;
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+		new CoveInventorySystem(); // initialize backend
 		// launch program
 		Application.launch(CoveApplication.class, args);
 	}
