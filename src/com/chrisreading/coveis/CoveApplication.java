510d62fce31a1d7ba3e91d1b0a559b827a79fbd6
nomad23541 <chrisreading.email@gmail.com>
2016-08-30 21:49:32 -0600

    Add confirmation dialog to gui

--- src/com/chrisreading/coveis/CoveApplication.java
+++ src/com/chrisreading/coveis/CoveApplication.java
@@ -3,6 +3,7 @@ package com.chrisreading.coveis;
 import java.io.IOException;
 
 import com.chrisreading.coveis.control.InventoryController;
+import com.chrisreading.coveis.control.dialog.ConfirmationDialogController;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class CoveApplication extends Application {
@@ -28,7 +30,7 @@ public class CoveApplication extends Application {
 	
 	public void initRootLayout() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(CoveInventorySystem.class.getResource("view//RootLayout.fxml"));
+		loader.setLocation(CoveInventorySystem.class.getResource("view/RootLayout.fxml"));
 		rootLayout = (BorderPane) loader.load();
 		
 		Scene scene = new Scene(rootLayout);
@@ -39,7 +41,7 @@ public class CoveApplication extends Application {
 	
 	public void showInventoryOverview() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(CoveInventorySystem.class.getResource("view//InventoryView.fxml"));
+		loader.setLocation(CoveInventorySystem.class.getResource("view/InventoryView.fxml"));
 		AnchorPane pane = (AnchorPane) loader.load();
 		
 		// set pane to the center of rootlayout
@@ -47,6 +49,32 @@ public class CoveApplication extends Application {
 		
 		// TODO: add controller
 		InventoryController controller = loader.getController();
+		controller.setApplication(this);
+	}
+	
+	public boolean showConfirmationDialog() throws IOException {
+		// load the fxml file
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(CoveInventorySystem.class.getResource("view/dialog/ConfirmationDialog.fxml"));
+		AnchorPane page = (AnchorPane) loader.load();
+				
+		// create the dialog stage
+		Stage dialogStage = new Stage();
+		dialogStage.setTitle("Are you sure?");
+		dialogStage.initModality(Modality.WINDOW_MODAL);
+		dialogStage.setResizable(false);
+		dialogStage.initOwner(primaryStage);
+		Scene scene = new Scene(page);
+		dialogStage.setScene(scene);
+		
+		// set the controller
+		ConfirmationDialogController controller = loader.getController();
+		controller.setDialogStage(dialogStage);
+		
+		// show the dialog and wait til the user closes it
+		dialogStage.showAndWait();
+		
+		return controller.isOkClicked();
 	}
 
 }
