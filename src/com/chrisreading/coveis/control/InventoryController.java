30a5155312286ad61b6ee8547764e79705859ee8
nomad23541 <chrisreading.email@gmail.com>
2016-08-30 21:49:59 -0600

    Add more functionality to InventoryController

--- src/com/chrisreading/coveis/control/InventoryController.java
+++ src/com/chrisreading/coveis/control/InventoryController.java
@@ -1,10 +1,15 @@
 package com.chrisreading.coveis.control;
 
+import java.io.IOException;
+
+import com.chrisreading.coveis.CoveApplication;
 import com.chrisreading.coveis.handler.InventoryManager;
 import com.chrisreading.coveis.model.Item;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -12,6 +17,9 @@ import javafx.scene.control.TableView;
  * Controller class to handle InventoryView.fxml
  */
 public class InventoryController {
+	
+	/** Instance of application class */
+	private CoveApplication ca;
 	
 	/** FXML */
 	@FXML
@@ -22,18 +30,54 @@ public class InventoryController {
 	private TableColumn<Item, Number> amountCol;
 	@FXML
 	private TableView<Item> table;
+	@FXML
+	private Button removeButton;
+	@FXML
+	private Button addButton;
 	
-	public InventoryController() {
-		
-	}
+	private ObservableList<Item> inventory = FXCollections.observableArrayList(InventoryManager.getInstance().getInventoryList());
+	
+	public InventoryController() {}
 
 	@FXML
 	public void initialize() {
-		table.setItems((ObservableList<Item>) InventoryManager.getInstance().getInventoryList());
+		table.setItems(inventory);
+		
 		// load table with loaded items in list
 		nameCol.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
 		priceCol.setCellValueFactory(cellData -> cellData.getValue().getPriceProperty());
 		amountCol.setCellValueFactory(cellData -> cellData.getValue().getAmountProperty());
 	}
+	
+	@FXML
+	private void handleAdd() {
+		// show add dialog
+		// temp
+		InventoryManager.getInstance().addItem(new Item("Hot Cheetos", 1.23, 50));
+	}
+	
+	@FXML
+	private void handleRemove() {
+		try {
+			boolean remove = ca.showConfirmationDialog();
+			
+			// if ok is clicked, remove the selected item
+			if(remove) {
+				InventoryManager.getInstance().removeItem(table.getSelectionModel().getSelectedItem());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// remove from list
+	}
+	
+	/**
+	 * Make this class access the rest of the gui
+	 */
+	public void setApplication(CoveApplication ca) {
+		this.ca = ca;
+	}
+	
+	
 	
 }
