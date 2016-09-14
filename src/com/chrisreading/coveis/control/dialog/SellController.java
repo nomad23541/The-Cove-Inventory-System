570cb506c8e6e7538a5093c4e7a85bb8b0643e65
nomad23541 <chrisreading.email@gmail.com>
2016-09-13 21:24:19 -0600

    Now functional

--- src/com/chrisreading/coveis/control/dialog/SellController.java
+++ src/com/chrisreading/coveis/control/dialog/SellController.java
@@ -2,9 +2,11 @@ package com.chrisreading.coveis.control.dialog;
 
 import com.chrisreading.coveis.model.Item;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 
 /**
  * Controller for sell dialog
@@ -23,15 +25,27 @@ public class SellController extends ADialogController {
 
 	@Override
 	protected void initialize() {
-		spinner
+		Platform.runLater(new Runnable() {
+			public void run() {
+				// set min to 1 and max to total amount
+				SpinnerValueFactory vf = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, item.getAmount());
+				spinner.setValueFactory(vf);
+				spinner.setEditable(false);
+			}
+		});
+
 	}
 	
+	@FXML
 	private void onScroll() {
-		
+		int amount = (Integer) spinner.getValue(); // get amount
+		double price = item.getPrice() * amount; // get total price
+		priceLabel.setText("$" + price);
 	}
 
 	@Override
 	protected void handleOk() {
+		item.setAmount(item.getAmount() - (Integer) spinner.getValue());
 		okClicked = true;
 		dialogStage.close();
 	}
@@ -39,6 +53,10 @@ public class SellController extends ADialogController {
 	@Override
 	protected void handleCancel() {
 		dialogStage.close();
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
 	}
 
 }
