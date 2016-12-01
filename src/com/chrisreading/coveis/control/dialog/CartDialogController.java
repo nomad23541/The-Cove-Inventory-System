87a0ee184c56c62e43990793f4cb7ba3d88dacc5
nomad23541 <chrisreading.email@gmail.com>
2016-12-01 12:51:16 -0700

    Started work on making cart dialog functional

--- src/com/chrisreading/coveis/control/dialog/CartDialogController.java
+++ src/com/chrisreading/coveis/control/dialog/CartDialogController.java
@@ -1,14 +1,18 @@
 package com.chrisreading.coveis.control.dialog;
 
+import com.chrisreading.coveis.CoveApplication;
 import com.chrisreading.coveis.model.Item;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 
 public class CartDialogController extends ADialogController {
 	
@@ -21,14 +25,15 @@ public class CartDialogController extends ADialogController {
 	@FXML
 	private TableColumn<Item, Number> amountCol;
 	@FXML
-	private ListView<Item> list;
+	private ListView<String> list;
 	@FXML
 	private Label priceLabel;
-	
+
 	private ObservableList<Item> inventory; // list of all items
+	private ObservableMap<String, Integer> cartList;
 
 	protected void initialize() {
-		table.setItems(inventory);
+		cartList = FXCollections.observableHashMap();
 		
 		// load table with loaded items in list
 		nameCol.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
@@ -37,9 +42,10 @@ public class CartDialogController extends ADialogController {
 		
 		Platform.runLater(new Runnable() {
 			public void run() {
+				table.setItems(inventory);
 				table.requestFocus();
 		        table.getSelectionModel().select(0);
-		        table.getFocusModel().focus(0);	
+		        table.getFocusModel().focus(0);
 			}
 		});
 	}
@@ -47,9 +53,30 @@ public class CartDialogController extends ADialogController {
 	public void setInventoryList(ObservableList<Item> inventory) {
 		this.inventory = inventory;
 	}
+	
+	/**
+	 * Add selected item to the list view
+	 * when double clicked.
+	 */
+	@FXML
+	protected void handleTableClick(MouseEvent e) {
+		Item item = table.getSelectionModel().getSelectedItem();
+		
+		if(e.getClickCount() == 2) {
+			if(item != null) {
+				if(!cartList.containsKey(item.getName())) {
+					cartList.put(item.getName(), 1);
+					System.out.println("Added " + item.getName() + item.getAmount() + " to hashmap");
+				} else {
+					int amount = cartList.get(item.getName());
+					cartList.put(item.getName(), amount += 1);
+					System.out.println("Added " + item.getName() + item.getAmount() + " to hashmap");
+				}
+			}
+		}
+	}
 
 	protected void handleOk() {
-		
 		okClicked = true;
 		dialogStage.close();
 	}
