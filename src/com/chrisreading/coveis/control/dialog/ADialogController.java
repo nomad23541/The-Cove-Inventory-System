ea1501eb771344bcdd5f204914d7681693506545
nomad23541 <chrisreading.email@gmail.com>
2016-12-10 14:52:02 -0700

    Allow dialogs access to CoveApplication instance
    changed from private to protected

--- src/com/chrisreading/coveis/control/dialog/ADialogController.java
+++ src/com/chrisreading/coveis/control/dialog/ADialogController.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 public abstract class ADialogController {
 	
 	/** Reference to application class */
-	private CoveApplication ca;
+	protected CoveApplication ca;
 	
 	/** Stage for this dialog */
 	protected Stage dialogStage;
